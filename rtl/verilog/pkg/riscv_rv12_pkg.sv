811d4501fd6a64280c3c26f9353dfb1274b7bb6d
Richard Herveille <richard.herveille@roalogic.com>
2018-02-02 09:55:12 +0100

    Initial release

---
+++ rtl/verilog/pkg/riscv_rv12_pkg.sv
@@ -0,0 +1,46 @@
+/////////////////////////////////////////////////////////////////
+//                                                             //
+//    ██████╗  ██████╗  █████╗                                 //
+//    ██╔══██╗██╔═══██╗██╔══██╗                                //
+//    ██████╔╝██║   ██║███████║                                //
+//    ██╔══██╗██║   ██║██╔══██║                                //
+//    ██║  ██║╚██████╔╝██║  ██║                                //
+//    ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝                                //
+//          ██╗      ██████╗  ██████╗ ██╗ ██████╗              //
+//          ██║     ██╔═══██╗██╔════╝ ██║██╔════╝              //
+//          ██║     ██║   ██║██║  ███╗██║██║                   //
+//          ██║     ██║   ██║██║   ██║██║██║                   //
+//          ███████╗╚██████╔╝╚██████╔╝██║╚██████╗              //
+//          ╚══════╝ ╚═════╝  ╚═════╝ ╚═╝ ╚═════╝              //
+//                                                             //
+//    RISC-V                                                   //
+//    RV12 Definitions Package                                 //
+//                                                             //
+/////////////////////////////////////////////////////////////////
+//                                                             //
+//             Copyright (C) 2014-2017 ROA Logic BV            //
+//             www.roalogic.com                                //
+//                                                             //
+//    Unless specifically agreed in writing, this software is  //
+//  licensed under the RoaLogic Non-Commercial License         //
+//  version-1.0 (the "License"), a copy of which is included   //
+//  with this file or may be found on the RoaLogic website     //
+//  http://www.roalogic.com. You may not use the file except   //
+//  in compliance with the License.                            //
+//                                                             //
+//    THIS SOFTWARE IS PROVIDED "AS IS" AND WITHOUT ANY        //
+//  EXPRESS OF IMPLIED WARRANTIES OF ANY KIND.                 //
+//  See the License for permissions and limitations under the  //
+//  License.                                                   //
+//                                                             //
+/////////////////////////////////////////////////////////////////
+
+
+package riscv_rv12_pkg;
+  parameter ARCHID       = 12;
+  parameter REVPRV_MAJOR = 1;
+  parameter REVPRV_MINOR = 10;
+  parameter REVUSR_MAJOR = 2;
+  parameter REVUSR_MINOR = 2;
+endpackage
+
