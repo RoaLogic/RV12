d63b3b33278c05aa98f46518a5e853dd8994eb53
Richard Herveille <richard.herveille@roalogic.com>
2018-06-11 10:21:55 +0200

    Minor structural changes

--- rtl/verilog/core/memory/riscv_memmisaligned.sv
+++ rtl/verilog/core/memory/riscv_memmisaligned.sv
@@ -1,84 +1,76 @@
-/////////////////////////////////////////////////////////////////
-//                                                             //
-//    ██████╗  ██████╗  █████╗                                 //
-//    ██╔══██╗██╔═══██╗██╔══██╗                                //
-//    ██████╔╝██║   ██║███████║                                //
-//    ██╔══██╗██║   ██║██╔══██║                                //
-//    ██║  ██║╚██████╔╝██║  ██║                                //
-//    ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝                                //
-//          ██╗      ██████╗  ██████╗ ██╗ ██████╗              //
-//          ██║     ██╔═══██╗██╔════╝ ██║██╔════╝              //
-//          ██║     ██║   ██║██║  ███╗██║██║                   //
-//          ██║     ██║   ██║██║   ██║██║██║                   //
-//          ███████╗╚██████╔╝╚██████╔╝██║╚██████╗              //
-//          ╚══════╝ ╚═════╝  ╚═════╝ ╚═╝ ╚═════╝              //
-//                                                             //
-//    RISC-V                                                   //
-//    Check if memory is aligned                               //
-//                                                             //
-/////////////////////////////////////////////////////////////////
-//                                                             //
-//             Copyright (C) 2014-2017 ROA Logic BV            //
-//             www.roalogic.com                                //
-//                                                             //
-//    Unless specifically agreed in writing, this software is  //
-//  licensed under the RoaLogic Non-Commercial License         //
-//  version-1.0 (the "License"), a copy of which is included   //
-//  with this file or may be found on the RoaLogic website     //
-//  http://www.roalogic.com. You may not use the file except   //
-//  in compliance with the License.                            //
-//                                                             //
-//    THIS SOFTWARE IS PROVIDED "AS IS" AND WITHOUT ANY        //
-//  EXPRESS OF IMPLIED WARRANTIES OF ANY KIND.                 //
-//  See the License for permissions and limitations under the  //
-//  License.                                                   //
-//                                                             //
-/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+//   ,------.                    ,--.                ,--.          //
+//   |  .--. ' ,---.  ,--,--.    |  |    ,---. ,---. `--' ,---.    //
+//   |  '--'.'| .-. |' ,-.  |    |  |   | .-. | .-. |,--.| .--'    //
+//   |  |\  \ ' '-' '\ '-'  |    |  '--.' '-' ' '-' ||  |\ `--.    //
+//   `--' '--' `---'  `--`--'    `-----' `---' `-   /`--' `---'    //
+//                                             `---'               //
+//    RISC-V                                                       //
+//    Misalignment Check                                           //
+//                                                                 //
+/////////////////////////////////////////////////////////////////////
+//                                                                 //
+//             Copyright (C) 2014-2018 ROA Logic BV                //
+//             www.roalogic.com                                    //
+//                                                                 //
+//     Unless specifically agreed in writing, this software is     //
+//   licensed under the RoaLogic Non-Commercial License            //
+//   version-1.0 (the "License"), a copy of which is included      //
+//   with this file or may be found on the RoaLogic website        //
+//   http://www.roalogic.com. You may not use the file except      //
+//   in compliance with the License.                               //
+//                                                                 //
+//     THIS SOFTWARE IS PROVIDED "AS IS" AND WITHOUT ANY           //
+//   EXPRESS OF IMPLIED WARRANTIES OF ANY KIND.                    //
+//   See the License for permissions and limitations under the     //
+//   License.                                                      //
+//                                                                 //
+////////////////////////////////////////////////////////////////////
+
+import biu_constants_pkg::*;
 
 module riscv_memmisaligned #(
-  parameter XLEN = 32
+  parameter XLEN    = 32,
+  parameter HAS_RVC = 0
 )
 (
-  input                   rstn,
-  input                   clk,
- 
-  //CPU side
-  input                   mem_req,
-  input      [XLEN  -1:0] mem_adr,
-  input      [       2:0] mem_size,
-  output reg              mem_misaligned,
+  input  logic              clk_i,
 
-  //To Upper layer
-  output reg              is_misaligned
+  //CPU side
+  input  logic              instruction_i,
+  input  logic              req_i,
+  input  logic [XLEN  -1:0] adr_i,
+  input  biu_size_t         size_i,
+
+  //To memory subsystem
+  output logic              misaligned_o
 );
   //////////////////////////////////////////////////////////////////
   //
   // Module Body
   //
-generate
-  if (XLEN == 32)
-    always_comb
-    case (mem_size)
-      BYTE   : is_misaligned = 1'b0;
-      HWORD  : is_misaligned =  mem_adr[  0];
-      WORD   : is_misaligned = |mem_adr[1:0];
-      default: is_misaligned = 1'b1;
-    endcase
-
-  if (XLEN == 64)
-    always_comb
-    case (mem_size)
-      BYTE   : is_misaligned = 1'b0;
-      HWORD  : is_misaligned =  mem_adr[  0];
-      WORD   : is_misaligned = |mem_adr[1:0];
-      DWORD  : is_misaligned = |mem_adr[2:0];
-      default: is_misaligned = 1'b1;
-    endcase
-endgenerate
+  logic misaligned;
 
 
-  always @(posedge clk)
-    if (mem_req) mem_misaligned <= is_misaligned;
-    else         mem_misaligned <= 1'b0;
+  //////////////////////////////////////////////////////////////////
+  //
+  // Module Body
+  //
+  always_comb
+    if (instruction_i)
+      misaligned = (HAS_RVC != 0) ? adr_i[0] : |adr_i[1:0];
+    else
+      unique case (size_i)
+        BYTE   : misaligned = 1'b0;
+        HWORD  : misaligned =  adr_i[  0];
+        WORD   : misaligned = |adr_i[1:0];
+        DWORD  : misaligned = |adr_i[2:0];
+        default: misaligned = 1'b1;
+      endcase
+
+
+  always @(posedge clk_i)
+    misaligned_o <= req_i & misaligned;
+
 endmodule
 
