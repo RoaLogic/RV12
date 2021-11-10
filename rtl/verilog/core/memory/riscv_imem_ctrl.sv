26a13acff8b8bdbb6a8fabfe2e219ec6b39b0193
Richard Herveille <richard.herveille@roalogic.com>
2021-11-10 20:46:46 +0100

    Initial commit

--- rtl/verilog/core/memory/riscv_imem_ctrl.sv
+++ rtl/verilog/core/memory/riscv_imem_ctrl.sv
@@ -70,9 +70,9 @@ module riscv_imem_ctrl #(
   input  logic          [XLEN            -1:0] imem_adr_i,
   output logic          [XLEN            -1:0] parcel_o,
   output logic          [XLEN/PARCEL_SIZE-1:0] parcel_valid_o,
-  output logic                                 err_o,
-                                               misaligned_o,
-                                               page_fault_o,
+  output logic                                 imem_error_o,
+                                               imem_misaligned_o,
+                                               imem_page_fault_o,
   input  logic                                 cache_flush_i,
   input  logic                                 dcflush_rdy_i,
 
@@ -115,16 +115,6 @@ module riscv_imem_ctrl #(
 
   //Misalignment check
   logic            misaligned;
-
-
-  //MMU signals
-  //Physical memory access signals
-  logic            preq;
-  logic [PLEN-1:0] padr;
-  biu_size_t       psize;
-  logic            plock;
-  biu_prot_t       pprot;
-  logic            page_fault;
   
 
   //from PMA check
@@ -150,13 +140,14 @@ module riscv_imem_ctrl #(
   // Module Body
   //
 
-  assign size         = XLEN == 64 ? DWORD : WORD;
-  assign prot         = biu_prot_t'( PROT_INSTRUCTION |
-	                             st_prv_i == PRV_U ? PROT_USER : PROT_PRIVILEGED );
-  assign lock         = 1'b0; //no locked instruction accesses
-  assign page_fault_o = 1'b0; //no MMU
+  assign size              = XLEN == 64 ? DWORD : WORD;   //Transfer size
+  assign prot              = biu_prot_t'( PROT_INSTRUCTION |
+	                                  st_prv_i == PRV_U ? PROT_USER : PROT_PRIVILEGED );
+  assign lock              = 1'b0; //no locked instruction accesses
+  assign imem_page_fault_o = 1'b0; //no MMU
 
   
+ 
   /* Hookup misalignment check
    */
   riscv_memmisaligned #(
@@ -164,13 +155,13 @@ module riscv_imem_ctrl #(
     .HAS_RVC ( HAS_RVC ) )
   misaligned_inst (
 //    .clk_i         ( clk_i      ),
-    .instruction_i ( 1'b1       ), //instruction access
-    .req_i         ( imem_req_i ),
-    .adr_i         ( imem_adr_i ),
-    .size_i        ( size       ),
-    .misaligned_o  ( misaligned ) );
+    .instruction_i ( 1'b1              ), //instruction access
+    .req_i         ( imem_req_i        ),
+    .adr_i         ( imem_adr_i        ),
+    .size_i        ( size              ),
+    .misaligned_o  ( misaligned        ) );
 
-   
+ 
   /* Hookup Physical Memory Atrributes Unit
    */
   riscv_pmachk #(
@@ -192,7 +183,7 @@ module riscv_imem_ctrl #(
     .adr_i          ( imem_adr_i      ),
     .size_i         ( size            ),
     .lock_i         ( lock            ),
-    .we_i           ( 1'b0            ),
+    .we_i           ( 1'b0            ), //Instruction bus doesn't write
 
     //Output
     .pma_o          (                 ),
@@ -222,6 +213,7 @@ module riscv_imem_ctrl #(
     .exception_o   ( pmp_exception ) );
 
 
+
   /* Hookup Cache
    */
 generate
@@ -230,49 +222,52 @@ generate
       /* Instantiate Instruction Cache Core
        */
       riscv_icache_core #(
-        .XLEN           ( XLEN             ),
-        .PLEN           ( PLEN             ),
-        .PARCEL_SIZE    ( PARCEL_SIZE      ),
-
-        .SIZE           ( CACHE_SIZE       ),
-        .BLOCK_SIZE     ( CACHE_BLOCK_SIZE ),
-        .WAYS           ( CACHE_WAYS       ),
-        .TECHNOLOGY     ( TECHNOLOGY       ) )
+        .XLEN                ( XLEN              ),
+        .PLEN                ( PLEN              ),
+	.HAS_RVC             ( HAS_RVC           ),
+        .PARCEL_SIZE         ( PARCEL_SIZE       ),
+        .SIZE                ( CACHE_SIZE        ),
+        .BLOCK_SIZE          ( CACHE_BLOCK_SIZE  ),
+        .WAYS                ( CACHE_WAYS        ),
+        .TECHNOLOGY          ( TECHNOLOGY        ) )
       icache_inst (
         //common signals
-        .rst_ni         ( rst_ni           ),
-        .clk_i          ( clk_i            ),
-        .clr_i          ( flush_i          ),
+        .rst_ni              ( rst_ni            ),
+        .clk_i               ( clk_i             ),
 
         //from PMA
-        .is_cacheable_i ( is_cacheble      ),
-        .mem_req_i      ( imem_req_i       ),
-        .mem_adr_i      ( imem_adr_i       ),
-//imem_flush?
-        .mem_size_i     ( size             ),
-        .mem_lock_i     ( lock             ),
-        .mem_prot_i     ( prot             ),
-        .mem_q_o        ( cache_q          ),
-        .mem_ack_o      ( cache_ack        ),
-        .mem_err_o      ( cache_err        ),
-        .flush_i        ( cache_flush_i    ),
-        .flushrdy_i     ( dcflush_rdy_i    ), //handled by stall_nxt_pc
+        .is_cacheable_i      ( is_cacheable      ),
+        .misaligned_i        ( pma_misaligned    ),
+        .mem_req_i           ( pma_req           ),
+	.mem_ack_o           ( imem_ack_o        ),
+        .mem_adr_i           ( imem_adr_i        ),
+        .mem_flush_i         ( imem_flush_i      ),
+        .mem_size_i          ( size              ),
+        .mem_lock_i          ( lock              ),
+        .mem_prot_i          ( prot              ),
+	.parcel_pc_o         (                   ),
+        .parcel_o            ( parcel_o          ),
+        .parcel_valid_o      ( parcel_valid_o    ),
+	.parcel_misaligned_o ( imem_misaligned_o ),
+        .parcel_error_o      ( imem_error_o      ),
+        .flush_i             ( cache_flush_i     ),
+        .flushrdy_i          ( dcflush_rdy_i     ),
 
         //To BIU
-        .biu_stb_o      ( biu_stb_o        ),
-        .biu_stb_ack_i  ( biu_stb_ack_i    ),
-        .biu_d_ack_i    ( biu_d_ack_i      ),
-        .biu_adri_o     ( biu_adri_o       ),
-        .biu_adro_i     ( biu_adro_i       ),
-        .biu_size_o     ( biu_size_o       ),
-        .biu_type_o     ( biu_type_o       ),
-	.biu_we_o       ( biu_we_o         ),
-        .biu_lock_o     ( biu_lock_o       ),
-        .biu_prot_o     ( biu_prot_o       ),
-        .biu_d_o        ( biu_d_o          ),
-        .biu_q_i        ( biu_q_i          ),
-        .biu_ack_i      ( biu_ack_i        ),
-        .biu_err_i      ( biu_err_i        ) );
+        .biu_stb_o           ( biu_stb_o         ),
+        .biu_stb_ack_i       ( biu_stb_ack_i     ),
+        .biu_d_ack_i         ( biu_d_ack_i       ),
+        .biu_adri_o          ( biu_adri_o        ),
+        .biu_adro_i          ( biu_adro_i        ),
+        .biu_size_o          ( biu_size_o        ),
+        .biu_type_o          ( biu_type_o        ),
+	.biu_we_o            ( biu_we_o          ),
+        .biu_lock_o          ( biu_lock_o        ),
+        .biu_prot_o          ( biu_prot_o        ),
+        .biu_d_o             ( biu_d_o           ),
+        .biu_q_i             ( biu_q_i           ),
+        .biu_ack_i           ( biu_ack_i         ),
+        .biu_err_i           ( biu_err_i         ) );
   end
   else  //No cache
   begin
@@ -281,44 +276,44 @@ generate
     * Control and glue logic only
     */
    riscv_noicache_core #(
-     .XLEN                   ( XLEN                  ),
-     .ALEN                   ( PLEN                  ),
-     .HAS_RVC                ( HAS_RVC               ),
-     .PARCEL_SIZE            ( PARCEL_SIZE           ) )
+     .XLEN                   ( XLEN                ),
+     .ALEN                   ( PLEN                ),
+     .HAS_RVC                ( HAS_RVC             ),
+     .PARCEL_SIZE            ( PARCEL_SIZE         ) )
    noicache_core_inst (
      //common signals
-     .rst_ni                 ( rst_ni                ),
-     .clk_i                  ( clk_i                 ),
+     .rst_ni                 ( rst_ni              ),
+     .clk_i                  ( clk_i               ),
 
      //CPU
-     .if_req_i               ( imem_req_i            ),
-     .if_ack_o               ( imem_ack_o            ),
-     .if_prot_i              ( prot                  ),
-     .if_flush_i             ( imem_flush_i          ),
-     .if_nxt_pc_i            ( imem_adr_i            ),
-     .if_parcel_pc_o         (                       ),
-     .if_parcel_o            ( parcel_o              ),
-     .if_parcel_valid_o      ( parcel_valid_o        ),
-     .if_parcel_misaligned_o ( parcel_misaligned_o   ),
-     .if_parcel_error_o      ( parcel_error_o        ),
-     .dcflush_rdy_i          ( dcflush_rdy_i         ),
-     .st_prv_i               ( st_prv_i              ),
+     .if_req_i               ( imem_req_i          ),
+     .if_ack_o               ( imem_ack_o          ),
+     .if_prot_i              ( prot                ),
+     .if_flush_i             ( imem_flush_i        ),
+     .if_nxt_pc_i            ( imem_adr_i          ),
+     .if_parcel_pc_o         (                     ),
+     .if_parcel_o            ( parcel_o            ),
+     .if_parcel_valid_o      ( parcel_valid_o      ),
+     .if_parcel_misaligned_o ( imem_misaligned_o   ),
+     .if_parcel_error_o      ( imem_error_o        ),
+     .dcflush_rdy_i          ( dcflush_rdy_i       ),
+     .st_prv_i               ( st_prv_i            ),
 
      //BIU
-     .biu_stb_o              ( biu_stb_o             ),
-     .biu_stb_ack_i          ( biu_stb_ack_i         ),
-     .biu_d_ack_i            ( biu_d_ack_i           ),
-     .biu_adri_o             ( biu_adri_o            ),
-     .biu_adro_i             ( biu_adro_i            ),
-     .biu_size_o             ( biu_size_o            ),
-     .biu_type_o             ( biu_type_o            ),
-     .biu_we_o               ( biu_we_o              ),
-     .biu_lock_o             ( biu_lock_o            ),
-     .biu_prot_o             ( biu_prot_o            ),
-     .biu_d_o                ( biu_d_o               ),
-     .biu_q_i                ( biu_q_i               ),
-     .biu_ack_i              ( biu_ack_i             ),
-     .biu_err_i              ( biu_err_i             ) );
+     .biu_stb_o              ( biu_stb_o           ),
+     .biu_stb_ack_i          ( biu_stb_ack_i       ),
+     .biu_d_ack_i            ( biu_d_ack_i         ),
+     .biu_adri_o             ( biu_adri_o          ),
+     .biu_adro_i             ( biu_adro_i          ),
+     .biu_size_o             ( biu_size_o          ),
+     .biu_type_o             ( biu_type_o          ),
+     .biu_we_o               ( biu_we_o            ),
+     .biu_lock_o             ( biu_lock_o          ),
+     .biu_prot_o             ( biu_prot_o          ),
+     .biu_d_o                ( biu_d_o             ),
+     .biu_q_i                ( biu_q_i             ),
+     .biu_ack_i              ( biu_ack_i           ),
+     .biu_err_i              ( biu_err_i           ) );
   end
 endgenerate
 
